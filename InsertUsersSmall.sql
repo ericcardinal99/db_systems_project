69a6d266962af2d4666dd0e3bf1e245f3e0b319c
Eric Cardinal <eric.cardinal@sjsu.edu>
2024-12-01 14:34:44 -0800

    fixed connection trigger

---
+++ InsertUsersSmall.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (username, profile_name, following, followers, date_created) VALUES
+('user1', 'user1', 0, 0, '2024-02-14 09:45:03'),
+('user2', 'user2', 0, 0, '2024-09-28 03:44:48'),
+('user3', 'user3', 0, 0, '2024-05-24 06:38:26'),
+('user4', 'user4', 0, 0, '2024-11-01 14:40:35'),
+('user5', 'user5', 0, 0, '2023-12-27 03:29:22')
